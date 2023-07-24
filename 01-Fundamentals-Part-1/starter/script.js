4b47cd8ec2c6be4e9275ffe01aff040ad2560847
Anshu Patel <anshupatel@Anshus-MacBook-Pro.local>
2023-07-25 02:05:12 +0530

    Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,26 @@
 let js = 'amazing';
-if (js == 'amazing') {
-    alert('Javascript is Fun');
-}
-
 console.log(40 + 8 - 2 + 23);
+
+console.log('Anshu');
+console.log(23);
+
+let firstName = 'Anshu';
+console.log(firstName);
+
+//Only use $ and _ as first letter for variable 
+let $functionVarName = 'Priyansh';
+let _functionVarName = 'Priyansh';
+
+let PI = 3.14;
+
+// let 3year = 3;
+
+// Name your variable Very descriptive 
+let myFirstJob = 'Software Engineer';
+let myCurrentJob = 'Software Engineer';
+
+//This is not a good practice
+let job1 = 'Teacher';
+let job2 = 'Programmer';
+
+console.log($functionVarName);
