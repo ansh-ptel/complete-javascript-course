b75cba05dbd26302804c4cd25164e97ae7f63025
Anshu Patel <anshupatel@Anshus-MacBook-Pro.local>
2023-07-25 02:08:33 +0530

    Assignment 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,26 +1,39 @@
-let js = 'amazing';
-console.log(40 + 8 - 2 + 23);
+// let js = 'amazing';
+// console.log(40 + 8 - 2 + 23);
 
-console.log('Anshu');
-console.log(23);
+// console.log('Anshu');
+// console.log(23);
 
-let firstName = 'Anshu';
-console.log(firstName);
+// let firstName = 'Anshu';
+// console.log(firstName);
 
-//Only use $ and _ as first letter for variable 
-let $functionVarName = 'Priyansh';
-let _functionVarName = 'Priyansh';
+// //Only use $ and _ as first letter for variable 
+// let $functionVarName = 'Priyansh';
+// let _functionVarName = 'Priyansh';
 
-let PI = 3.14;
+// let PI = 3.14;
 
-// let 3year = 3;
+// // let 3year = 3;
 
-// Name your variable Very descriptive 
-let myFirstJob = 'Software Engineer';
-let myCurrentJob = 'Software Engineer';
+// // Name your variable Very descriptive 
+// let myFirstJob = 'Software Engineer';
+// let myCurrentJob = 'Software Engineer';
 
-//This is not a good practice
-let job1 = 'Teacher';
-let job2 = 'Programmer';
+// //This is not a good practice
+// let job1 = 'Teacher';
+// let job2 = 'Programmer';
 
-console.log($functionVarName);
+// console.log($functionVarName);
+
+// A S S I G N M E N T - 1
+
+// Declare variables called'country','continent'and'population'and assign their values according to your own country (population in millions)
+let country = 'India';
+let continent = 'Asia';
+let population = 'Too Much';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
